8eb57e0ba7b87cd09c97edfb592a8fe2479ecb29
Thomas ULRICI <t.ulrici@it-akademy.fr>
2024-08-09 14:01:21 +0200

    refactor:

--- src/components/League/LeagueItem.js
+++ src/components/League/LeagueItem.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import { Text, StyleSheet, View, TouchableOpacity } from 'react-native';
 
-export default function LeagueItem({ name, navigation }) {
+export default function LeagueItem({ name, league, navigation }) {  // Accept 'league' as a prop
 
     function first() {
         //TODO BACKEND: return the first(s) of the league
-        return (<Text style={styles.textContainer}>JeanMich(s) is currently first of the league with n points !</Text>);
+        return (
+            <Text style={styles.textContainer}>
+                JeanMich(s) is currently first of the league with n points!
+            </Text>
+        );
     }
 
+    // GameVueLeague link doesn't work for the dead leagues yet because the games don't have the league attribute yet
     return (
         <View style={styles.itemContainer}>
-
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('GameVueLeague')}>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => navigation.navigate('GameVueLeague', { league })}  // Pass 'league' as a param
+            >
                 <Text style={styles.ligueName}>{name}</Text>
                 {first()}
-                <Text style={styles.buttonText}>Voir les games de la ligue</Text>
+                <Text style={styles.buttonText}>See the league's games</Text>
             </TouchableOpacity>
-
         </View>
     );
-
 }
 
 const styles = StyleSheet.create({
